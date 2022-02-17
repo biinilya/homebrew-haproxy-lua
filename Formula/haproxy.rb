ba187f2dcda20befbdfc33658ded396e8c339894
Ilya Biin <me@ilyabiin.com>
2022-02-17 21:22:33 +0800

    Update haproxy.rb
    formula sync to upstream

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -6,16 +6,16 @@ class Haproxy < Formula
     depends_on "openssl@1.1"
     depends_on "pcre"
     depends_on "lua"
+    
+    uses_from_macos "zlib"
 
     def install
       lua = Formula["lua"]
       args = %W[
         TARGET=osx
-        USE_KQUEUE=1
-        USE_POLL=1
-        USE_PCRE=1
+        USE_PCRE2=1
+        USE_PCRE2_JIT=1
         USE_OPENSSL=1
-        USE_THREAD=1
         USE_ZLIB=1
         ADDLIB=-lcrypto
         USE_LUA=1
@@ -25,39 +25,19 @@ class Haproxy < Formula
       ]
 
       # We build generic since the Makefile.osx doesn't appear to work
-      system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "LDFLAGS=#{ENV.ldflags}", *args
+      system "make", *args
       man1.install "doc/haproxy.1"
       bin.install "haproxy"
     end
 
-    plist_options :manual => "haproxy -f #{HOMEBREW_PREFIX}/etc/haproxy.cfg"
-
-    def plist
-      <<~EOS
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
-          <dict>
-            <key>Label</key>
-            <string>#{plist_name}</string>
-            <key>KeepAlive</key>
-            <true/>
-            <key>ProgramArguments</key>
-            <array>
-              <string>#{opt_bin}/haproxy</string>
-              <string>-f</string>
-              <string>#{etc}/haproxy.cfg</string>
-            </array>
-            <key>StandardErrorPath</key>
-            <string>#{var}/log/haproxy.log</string>
-            <key>StandardOutPath</key>
-            <string>#{var}/log/haproxy.log</string>
-          </dict>
-        </plist>
-      EOS
+    service do
+      run [opt_bin/"haproxy", "-f", etc/"haproxy.cfg"]
+      keep_alive true
+      log_path var/"log/haproxy.log"
+      error_log_path var/"log/haproxy.log"
     end
 
     test do
       system bin/"haproxy", "-v"
     end
-  end
+end
